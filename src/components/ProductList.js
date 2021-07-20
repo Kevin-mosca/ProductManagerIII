ea6bdfbe05e002d5a8d68c7c09bebc949aeb616f
Kevin-mosca <Kevinmoscaa@gmail.com>
2021-07-20 12:21:05 -0700

    Comit 1

---
+++ src/components/ProductList.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Link } from '@reach/router';
+import axios from 'axios'
+
+export default props => {
+    const { removeFromDom } = props;
+    const deleteProduct = (productId) => {
+        axios.delete('http://localhost:8000/api/product/' + productId)
+            .then(res => {
+                removeFromDom(productId)
+            })
+    }
+    return(
+        <div>
+            {props.product.map((product, index)=>{
+                return <p key={index}><Link to ={"/product/" + product._id}> {product.title}</Link>
+                |
+                <button onClick={(e)=>{deleteProduct(product._id)}}>
+                    Delete
+                </button>
+                </p>
+            })}
+        </div>
+    )
+}
---
+++ src/views/Main.js
@@ -0,0 +1,30 @@
+import axios from 'axios';
+import React, {useEffect, useState } from 'react';
+
+import ProductForm from '../components/ProductForm';
+import ProductList from '../components/ProductList';
+
+export default () => {
+    const [product, setProduct] = useState([]);
+    const [loaded, setLoaded] = useState(false);
+
+    useEffect(()=>{
+        axios.get('http://localhost:8000/api/product')
+            .then(res=>{
+                setProduct(res.data);
+                setLoaded(true);
+            });
+            
+    },[]);
+
+    const removeFromDom = productId => {
+        setProduct(product.filter(product => product._id != productId));
+    }
+    return(
+        <div>
+            <ProductForm/>
+            <hr/>
+            {loaded && <ProductList product={product} removeFromDom={removeFromDom}/>}
+        </div>
+    )
+}
